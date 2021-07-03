cebef3ea1ecbcbf3c96b3020da321628dd853827
Pedro Assunção <pedro.assuncao97@hotmail.com>
2021-07-03 19:56:44 -0300

    Primeira rota

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ const express = require('express')
 
 const server = express()
 
+server.get('/', (req, res) => {
+    return res.json({ ok: true })
+})
+
 server.listen(3333, () => console.log('server up 3333'))
