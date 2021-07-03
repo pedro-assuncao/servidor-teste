fd49131d2aeb2a7a665f4623a497ab70c54d2c8f
Pedro Assunção <pedro.assuncao97@hotmail.com>
2021-07-03 20:06:09 -0300

    Adicionando mensagem no retorno

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const server = express()
 
 server.get('/', (req, res) => {
-    return res.json({ ok: true })
+    return res.json({ ok: true, message: 'Hello world' })
 })
 
 server.listen(3333, () => console.log('server up 3333'))
